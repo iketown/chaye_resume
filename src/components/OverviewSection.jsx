aa63e99474ebc468cfaa70b25793f4603b210a55
ike76 <ike76@me.com>
2020-05-08 06:39:17 -0500

    new intro paragraph

--- src/components/OverviewSection.jsx
+++ src/components/OverviewSection.jsx
@@ -18,22 +18,42 @@ const OverviewSection = () => {
           <a href={`mailto:${config.email}`}>{config.email}</a>
         </div>
         <div className="mb-5">
-          <p className="lead">
-            My name is Chaye Eichenberger, nice to meet you!
-          </p>
+          <p>My name is Chaye Eichenberger, nice to meet you!</p>
           <p>
-            I have been in Sales, Business Development, and Account Management
+            I have been in <b>Sales</b>, <b>Business Development</b>, and{' '}
+            <b>Account Management</b>
             for 16+ years. I’ve worked for large companies like Gaylord Hotels
             and American Express, as well as start ups such as Emma and
-            GoNoodle. I enjoy the entrepreneurial spirit found in smaller
-            companies and try to bring that fresh perspective and excitement to
-            every role.
+            GoNoodle.
           </p>
           <p>
-            I believe my biggest contributions to a company are my ability to{' '}
-            <b>collaborate</b> and bring teams together, define <b>strategy</b>{' '}
-            and <b>differentiate</b> a product or service, develop processes to{' '}
-            <b>drive efficiency</b>, and build strong client relationships.
+            My experience spans multiple industries, including hospitality,
+            Saas, travel, entertainment, and media.
+          </p>
+          <p>
+            While working in <b>larger companies</b>, I’ve come to understand
+            the benefits of corporate structure and processes, the cadence of
+            building business, and the requirements for making change.
+          </p>
+          <p>
+            Through my time spent with <b>startups</b>, I have grown familiar
+            with the drive for innovation, the need to be nimble, and the
+            entrepreneurial spirit it takes to stay motivated.
+          </p>
+          <p>
+            I recognize the benefits and limitations that both face and know how
+            to successfully navigate each. I believe my biggest contributions to
+            a company are my ability to <b>collaborate</b> and bring teams
+            together, define strategy and <b>differentiate</b> a product or
+            service, develop processes to drive <b>efficiency</b>, and build
+            strong client <b>relationships</b>.
+          </p>
+          <p>
+            I have experience with selling and actualizing business, allowing me
+            to understand both top and bottom line impact. I have led diverse
+            teams; managed large, cross-departmental projects; and spearheaded
+            the ideation, creation, and execution of sales strategies, bespoke
+            campaigns, and new products for market.
           </p>
         </div>
 
